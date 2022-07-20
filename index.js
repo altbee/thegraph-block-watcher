9b75b8c4bc76c9bee919bd80d57acd662b9dabde
Altbee <liaojikundever@gmail.com>
2022-07-20 08:19:46 -0500

    block watcher

---
+++ index.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+
+const schedule = require("node-schedule");
+const pgp = require("pg-promise")();
+const { providers } = require("ethers");
+
+const PSQL_URL = process.env.PSQL_URL;
+const DEPLOYMENT = process.env.DEPLOYMENT;
+
+const { JsonRpcProvider } = providers;
+const provider = new JsonRpcProvider("https://evmexplorer.velas.com/rpc", 106);
+
+// https://stackoverflow.com/questions/9205496/how-to-make-connection-to-postgres-via-node-js
+
+let latestBlockNumber = 0;
+
+const checkAndUpdate = async () => {
+  const db = pgp(PSQL_URL);
+
+  const result = await db.one(
+    "SELECT latest_ethereum_block_number FROM subgraphs.subgraph_deployment WHERE deployment = $1",
+    [DEPLOYMENT]
+  );
+  const curBlockNumber = Number(result.latest_ethereum_block_number);
+
+  console.log("prevBlockNumber", latestBlockNumber);
+  console.log("curBlockNumber", curBlockNumber);
+
+  if (latestBlockNumber === 0) {
+    latestBlockNumber = curBlockNumber;
+  } else if (latestBlockNumber === curBlockNumber) {
+    // need to get block info
+    console.log("===should update===");
+
+    const block = await provider.getBlock(curBlockNumber - 1);
+    const blockHash = block.hash;
+
+    console.log(
+      `===previous number: ${curBlockNumber - 1}: hash ${blockHash}===`
+    );
+
+    await db.tx((t) => {
+      return t.none(
+        `update subgraphs.subgraph_deployment sd
+set latest_ethereum_block_hash = decode($1, 'hex'),
+latest_ethereum_block_number = $2
+where sd.deployment = $3`,
+        [blockHash.substring(2), curBlockNumber - 1, DEPLOYMENT]
+      );
+    });
+  }
+};
+
+const main = async () => {
+  schedule.scheduleJob("0 * * * *", function () {
+    console.log(`======${new Date().toTimeString()}======`);
+    // every hour
+    checkAndUpdate()
+      .then(() => {
+        console.log("====Done====");
+      })
+      .catch((err) => {
+        console.error("===", err);
+      });
+  });
+};
+
+main();
